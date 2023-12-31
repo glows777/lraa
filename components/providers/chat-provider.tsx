11485ae04c1028ceb4aa7c12fd1d998a12c7ea97
glows777 <1914426389@qq.com>
2024-01-01 00:56:49 +0800

    feat: chat context to manage the message sending

---
+++ components/providers/chat-provider.tsx
@@ -0,0 +1,212 @@
+import { useMutation } from '@tanstack/react-query'
+import {
+  ChangeEvent,
+  FC,
+  ReactNode,
+  createContext,
+  useRef,
+  useState,
+} from 'react'
+
+import { trpc } from '@/app/_trpc/client'
+import { toast } from '@/components/ui/use-toast'
+import { INFINITE_QUERY_LIMIT } from '@/constant'
+
+interface ChatProviderProps {
+  children: ReactNode
+  fileId: string
+}
+
+interface StreamResponse {
+  addMessage: () => void
+  handleInputChange: (e: ChangeEvent<HTMLTextAreaElement>) => void
+  isLoading: boolean
+  message: string
+}
+
+export const ChatContext = createContext<StreamResponse>({
+  addMessage: () => {},
+  handleInputChange: () => {},
+  isLoading: false,
+  message: '',
+})
+
+const ChatProvider: FC<ChatProviderProps> = ({ children, fileId }) => {
+  const [message, setMessage] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  const backupMessage = useRef('')
+
+  const utils = trpc.useUtils()
+
+  const { mutate: sendMessage } = useMutation({
+    mutationFn: async ({ message }: { message: string }) => {
+      const response = await fetch('/api/message', {
+        method: 'POST',
+        body: JSON.stringify({
+          fileId,
+          message,
+        }),
+      })
+
+      if (!response.ok) {
+        throw new Error('Failed to send Message')
+      }
+
+      return response.body
+    },
+    onMutate: async ({ message }) => {
+      backupMessage.current = message
+      setMessage('')
+
+      await utils.getFileMessages.cancel()
+      const previousMessage = utils.getFileMessages.getInfiniteData()
+      utils.getFileMessages.setInfiniteData(
+        {
+          fileId,
+          limit: INFINITE_QUERY_LIMIT,
+        },
+        (old) => {
+          if (!old) {
+            return {
+              pages: [],
+              pageParams: [],
+            }
+          }
+
+          let newPages = [...old.pages]
+          let latestPage = newPages[0]!
+
+          latestPage.messages = [
+            ...latestPage.messages,
+            {
+              createdAt: new Date().toISOString(),
+              id: crypto.randomUUID(),
+              content: message,
+              isUserMessage: true,
+            },
+          ]
+          newPages[0] = latestPage
+
+          return {
+            ...old,
+            pages: newPages,
+          }
+        }
+      )
+
+      setIsLoading(true)
+      return {
+        previousMessage:
+          previousMessage?.pages.flatMap((page) => page.messages) ?? [],
+      }
+    },
+    onSuccess: async (stream) => {
+      if (!stream) {
+        return toast({
+          title: 'There was a problem sending this message',
+          description: 'Please refresh this page and try again',
+          variant: 'destructive',
+        })
+      }
+
+      const reader = stream.getReader()
+      const decoder = new TextDecoder()
+      let done = false
+
+      let accResponse = ''
+      while (!done) {
+        const { value, done: doneReading } = await reader.read()
+        done = doneReading
+        const chunkValue = decoder.decode(value)
+
+        accResponse += chunkValue
+
+        utils.getFileMessages.setInfiniteData(
+          {
+            fileId,
+            limit: INFINITE_QUERY_LIMIT,
+          },
+          (old) => {
+            if (!old) {
+              return {
+                pages: [],
+                pageParams: [],
+              }
+            }
+
+            let isAiResponseCreated = old.pages.some((page) =>
+              page.messages.some((msg) => msg.id === 'ai-response')
+            )
+
+            let updatedPages = old.pages.map((page) => {
+              if (page === old.pages[0]) {
+                let updatedMessages
+
+                if (!isAiResponseCreated) {
+                  updatedMessages = [
+                    ...page.messages,
+                    {
+                      createdAt: new Date().toISOString(),
+                      id: 'ai-response',
+                      content: accResponse,
+                      isUserMessage: false,
+                    },
+                  ]
+                } else {
+                  updatedMessages = page.messages.map((msg) => {
+                    if (msg.id === 'ai-response') {
+                      return {
+                        ...msg,
+                        content: accResponse,
+                      }
+                    }
+                    return msg
+                  })
+                }
+                return {
+                  ...page,
+                  messages: updatedMessages
+                }
+              }
+              return page
+            })
+
+            return { ...old, pages: updatedPages }
+          }
+        )
+      }
+    },
+    onError: (_, __, context) => {
+      setMessage(backupMessage.current)
+      utils.getFileMessages.setData(
+        { fileId },
+        { messages: context?.previousMessage ?? [] }
+      )
+    },
+    onSettled: async () => {
+      setIsLoading(false)
+      await utils.getFileMessages.invalidate({ fileId })
+    },
+  })
+
+  const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value)
+  }
+  const addMessage = () => sendMessage({ message })
+
+  return (
+    <ChatContext.Provider
+      value={{
+        addMessage,
+        message,
+        handleInputChange,
+        isLoading,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  )
+}
+
+export default ChatProvider
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { z } from 'zod'
 
+import { INFINITE_QUERY_LIMIT } from '@/constant'
 import { db } from '@/db'
 import { TRPCError } from '@trpc/server'
 
@@ -96,6 +97,59 @@ export const appRouter = router({
       }
 
       return { status: file.status }
+    }),
+
+  getFileMessages: privateProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(99).nullish(),
+        cursor: z.string().nullish(),
+        fileId: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { userId } = ctx
+      const { fileId, cursor } = input
+      const limit = input.limit ?? INFINITE_QUERY_LIMIT
+
+      const file = await db.file.findFirst({
+        where: {
+          id: fileId,
+          userId,
+        },
+      })
+
+      if (!file) {
+        throw new TRPCError({ code: 'NOT_FOUND' })
+      }
+
+      const messages = await db.message.findMany({
+        take: limit + 1,
+        where: {
+          fileId,
+        },
+        orderBy: {
+          createdAt: 'asc',
+        },
+        cursor: cursor ? { id: cursor } : undefined,
+        select: {
+          id: true,
+          isUserMessage: true,
+          createdAt: true,
+          content: true,
+        },
+      })
+
+      let nextCursor: typeof cursor | undefined = undefined
+      if (messages.length > limit) {
+        const nextItem = messages.pop()
+        nextCursor = nextItem?.id
+      }
+
+      return {
+        messages,
+        nextCursor,
+      }
     }),
 })
 
